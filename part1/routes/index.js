7c174b2280baad4955a4fb205e32f59be8e5a00a
sirjoshie <hsoj06@hotmail.com>
2025-06-20 04:17:57 +0000

    [automatic commit] 2025-06-20 04:17:57 UTC 103.237.18.46

--- part1/routes/index.js
+++ part1/routes/index.js
@@ -22,8 +22,7 @@ router.get('/dogs', async (req, res) => {
 router.get('/walkrequests/open', async (req, res) => {
   try {
     const [rows] = await req.db.query(`
-      SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes,
-             wr.location, u.username AS owner_username
+      SELECT wr.request_id, d.name AS dog_name, wr.requested_time, wr.duration_minutes, wr.location, u.username AS owner_username
       FROM WalkRequests wr
       JOIN Dogs d ON wr.dog_id = d.dog_id
       JOIN Users u ON d.owner_id = u.user_id
