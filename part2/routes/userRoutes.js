ce4a9409651e0ec2d8f67857ae4e446ed1c28563
sirjoshie <hsoj06@hotmail.com>
2025-06-21 03:27:27 +0000

    [automatic commit] 2025-06-21 03:27:27 UTC 103.237.18.7

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -53,6 +53,7 @@ router.post('/login', async (req, res) => {
 
     res.json({ message: 'Login successful', user: rows[0] });
   } catch (error) {
+    console.error('Login Error:', error);
     res.status(500).json({ error: 'Login failed' });
   }
 });
