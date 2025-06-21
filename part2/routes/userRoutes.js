c8a681d392e8bef938c94723f9de2d3c38e9696c
sirjoshie <hsoj06@hotmail.com>
2025-06-21 04:56:20 +0000

    [automatic commit] 2025-06-21 04:56:20 UTC 103.237.18.7

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -68,10 +68,6 @@ router.post('/logout', (req, res) => {
 });
 
 router.get('/mydogs', async (req, res) => {
-  if (!req.session.user || req.session.user.role !== 'owner') {
-    return res.status(401).json({ error: 'Unauthorized' });
-  }
-
   const ownerId = req.session.user.user_id;
 
   try {
